4ad41077da95cd7e411ec10af6f8ddeb0b3d40a3
Stephanie Smith <stephanie.lauren.design@gmail.com>
2019-01-31 08:16:11 -0800

    put request

--- src/app/servers.service.ts
+++ src/app/servers.service.ts
@@ -6,7 +6,8 @@ export class ServerService {
   constructor(private http: Http) {}
 
   storeService(servers: any[]) {
-    return this.http.post('https://udemy-ng-http-alchemy.firebaseio.com/data.json', servers);
+    // return this.http.post('https://udemy-ng-http-alchemy.firebaseio.com/data.json', servers);
+    return this.http.put('https://udemy-ng-http-alchemy.firebaseio.com/data.json', servers);
   }
 
   getServers() {
